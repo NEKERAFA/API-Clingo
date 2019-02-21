77edef33bc9b327ff1192077801e64a1115b36bb
trigork <trigork@gmail.com>
2019-02-21 19:08:20 +0100

    Fixed errors, displays formulae again and work as intended.

--- examples/mccluskey-petrick/minterms.py
+++ examples/mccluskey-petrick/minterms.py
@@ -24,8 +24,43 @@ def input_to_asp(input_file):
 def symbols_to_facts(symbols):
     return " ".join(["{0}.".format(sym) for sym in symbols])
 
+def implicates_to_dict(symbols, prefix):
+    impdict = {}
+    for s in symbols:
+        if s.name == (prefix+"impl"):
+            k, a, v = s.arguments[0], s.arguments[1], s.arguments[2]
+            if not k in impdict.keys():
+                impdict[k] = { a: v }
+            else:
+                impdict[k][a] = v
+    return impdict
+
+def implicates_dict_str(impdict):
+    ret = ""
+    for i,k in enumerate(impdict.keys()):
+        ret += "[{0}] {1}: ".format(i,k)
+        for kk in sorted(impdict[k]):
+            ret += str(impdict[k][kk])
+        ret += "\n"
+    return ret
+
+def implicates_dict_formula(impdict):
+    terms = []
+    for k in impdict.keys():
+        term = []
+        for kk in sorted(impdict[k]):
+            ts = ""
+            v = str(impdict[k][kk])
+            if v == "0":
+                ts += "not "
+            if v != "x":
+                ts += str(kk)
+                term += [ts]
+        terms += ["(" + " ^ ".join(term) + ")"]
+    return " v ".join(terms)
+
 def solve(asp_program, asp_facts):
-    c = clingo.Control()
+    c = clingo.Control(["0"])
     c.load("./asp/"+asp_program+".lp")
     for facts in asp_facts:
         c.add("base", [], facts)
@@ -33,7 +68,7 @@ def solve(asp_program, asp_facts):
     ret = []
     with c.solve(yield_=True) as handle:
         for m in handle:
-            ret = m.symbols(shown=True)
+            ret += [m.symbols(shown=True)]
     return ret
 
 def solve_iter(asp_program, asp_facts):
@@ -76,16 +111,27 @@ def main():
     mincover_syms = solve_iter("min-cover", [input_facts, primpl_facts])
     mincover_facts = symbols_to_facts(mincover_syms)
 
+    essndict, finaldicts = {}, []
     # If the minimal coverage doesn't cover all minterms, petrick it
     if not any(sym.name == "fullcover" for sym in mincover_syms):
-        petrick_syms = solve("petrick", [mincover_facts])
-        petrick_facts = symbols_to_facts(petrick_syms)
-
-    print(mincover_facts)
-    print(petrick_facts)
-    print("FINISHED")
-
-
+        print("SECONDARY IMPLICATES")
+        petrick_solutions = solve("petrick", [mincover_facts])
+        for idx,petrick_syms in enumerate(petrick_solutions):
+            petrick_facts = symbols_to_facts(petrick_syms)
+            if any(sym.name == "selectimplid" for sym in petrick_syms):
+                secdict = implicates_to_dict(petrick_syms, "select")
+                finaldictasy = { **essndict, **secdict }
+                finaldicts += [ finaldictasy ]
+                print("SOLUTION #{0}".format(idx))
+                print(implicates_dict_str(finaldictasy))
+                print(implicates_dict_formula(finaldictasy) + "\n")
+            else:
+                print("NO SECONDARY IMPLICATES")
+    else:
+        print("ESSENTIAL IMPLICATES")
+        essndict = implicates_to_dict(mincover_syms, "essn")
+        print(implicates_dict_str(essndict))
+        print(implicates_dict_formula(essndict))
 
 
 if __name__ == "__main__":
